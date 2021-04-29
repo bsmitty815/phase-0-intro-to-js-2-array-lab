fa03b5944ebb1e70e0ded8ac636fc4a478bd236a
Brian Smith <brian.bubbam@gmail.com>
2021-04-29 18:43:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('name');
+}
+
+function appendCat(name) {
+ const catsCopy = [...cats, "Broom"];
+ return catsCopy;
+}
+
+function prependCat(name) {
+    const catsCopy = ["Arnold", ...cats];
+    return catsCopy;
+   }
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
